4c812db6cf95937d16e6cfbde460808692682075
Lê Bật Hoàng <hoanglb2k3@gmail.com>
2026-01-16 10:46:47 +0700

    feat: add resend activation account feature

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ const cors = require("cors");
 const app = express();
 
 const authRoutes = require("./src/routes/AuthRouter");
+const userRoutes = require("./src/routes/UserRouter");
 
 app.use(
   cors({
     origin: "http://localhost:3001",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 app.get("/", (req, res) => res.send("Auth API is running"));
 
 app.listen(3000, () => {
--- src/routes/AuthRouter.js
+++ src/routes/AuthRouter.js
@@ -5,9 +5,10 @@ const AuthController = require('../controller/AuthController');
 
 router.post("/register", AuthController.register);
 router.post("/login", AuthController.login);
-router.post("/active-account", AuthController.activateAccount);
-router.post("/forgot-password", userRouter.forgotPassword);
-router.post("/reset-password", userRouter.resetPassword);
+router.post("/activate-account", AuthController.activateAccount);
+router.post("/resend-activation", AuthController.resendActivation);
+router.post("/forgot-password", AuthController.forgotPassword);
+router.post("/reset-password", AuthController.resetPassword);
 
 
 module.exports = router;
